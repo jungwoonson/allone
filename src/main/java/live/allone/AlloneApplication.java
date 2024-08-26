c41929f028f0f422e322bfbad3a876877642cb40
jungwoonson <jungwoon727@gmail.com>
2024-08-26 17:27:31 +0900

    first commit

---
+++ src/main/java/live/allone/AlloneApplication.java
@@ -0,0 +1,13 @@
+package live.allone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlloneApplication.class, args);
+    }
+
+}
